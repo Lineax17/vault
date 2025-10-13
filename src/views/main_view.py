a7bb49d74fd9ccc353dc7da7be0f588f3dc0cd45
Lineax17 <philipp.hartmann04@gmail.com>
2025-10-13 19:45:47 +0200

    add first concept gui

--- src/views/main_view.py
+++ src/views/main_view.py
@@ -4,14 +4,32 @@ from tkinter import ttk
 class MainView:
     def __init__(self, root):
         self.root = root
-        self.root.title("Main View")
-        self.root.geometry("400x300")
+        self.root.title("Vault - Password Manager")
+        self.root.geometry("550x800")
 
-        self.label = Label(self.root, text="Welcome to the Main View")
+        # New Entry Section
+        self.label = Label(self.root, text="New Entry")
         self.label.pack(pady=20)
 
-        self.button = Button(self.root, text="Click Me", command=self.on_button_click)
+        self.label = Label(self.root, text="Username:")
+        self.label.pack(pady=10)
+
+        self.username = Entry(self.root)
+        self.username.pack(pady=10)
+
+        self.label = Label(self.root, text="Password:")
+        self.label.pack(pady=10)
+
+        self.password = Entry(self.root)
+        self.password.pack(pady=10)
+
+        self.button = Button(self.root, text="Save", command=self.on_button_click)
         self.button.pack(pady=10)
 
+        # Existing Entries Section
+        self.label = Label(self.root, text="Existing Entries")
+        self.label.pack(pady=20)
+
     def on_button_click(self):
-        print("Button clicked!")
+        print(Entry.get(self.username))
+        print(Entry.get(self.password))
