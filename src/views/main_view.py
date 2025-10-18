b9b72c70555af3c68f16328f1ac168b0156b11b2
Lineax17 <philipp.hartmann04@gmail.com>
2025-10-18 12:25:05 +0200

    add json read and write functions

--- src/views/main_view.py
+++ src/views/main_view.py
@@ -1,5 +1,7 @@
+from readline import get_current_history_length
 from tkinter import *
 from tkinter import ttk
+import json
 
 class MainView:
     def __init__(self, root):
@@ -10,6 +12,12 @@ class MainView:
         # New Entry Section
         self.label = Label(self.root, text="New Entry")
         self.label.pack(pady=20)
+
+        self.label = Label(self.root, text="Name:")
+        self.label.pack(pady=10)
+
+        self.name = Entry(self.root)
+        self.name.pack(pady=10)
 
         self.label = Label(self.root, text="Username:")
         self.label.pack(pady=10)
@@ -30,6 +38,35 @@ class MainView:
         self.label = Label(self.root, text="Existing Entries")
         self.label.pack(pady=20)
 
+
+
     def on_button_click(self):
+        print(Entry.get(self.name))
         print(Entry.get(self.username))
         print(Entry.get(self.password))
+
+        data = {
+            "name": Entry.get(self.name),
+            "username": Entry.get(self.username),
+            "password": Entry.get(self.password)
+        }
+
+        self.write_entry(data)
+
+    def read_entry(self, id_number):
+        with open('src/passwords.json', 'r') as file:
+            data = json.load(file)
+            return data
+
+    def write_entry(self, entry):
+        try:
+            with open('src/passwords.json', 'r') as file:
+                data = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = {
+                "entries": []
+            }
+        data["entries"].append(entry)
+
+        with open('src/passwords.json', 'w') as file:
+            json.dump(data, file)
